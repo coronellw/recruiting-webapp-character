d449162ebccd53557fa2b683603ed58ddb4b9661
Wiston Coronell <coronell@gmail.com>
2025-02-17 23:39:46 -0400

    point 3 completed

--- src/components/class-type/classes-type.tsx
+++ src/components/class-type/classes-type.tsx
@@ -1,7 +1,8 @@
 import classNames from "classnames";
 import { Attributes, Class } from "../../types";
 
-import "./classes-type.css"
+import "./classes-type.css";
+import { useState, useRef } from "react";
 
 type ClassesProps = {
   characterAttributes: Attributes;
@@ -14,16 +15,56 @@ export default function ClassType({
   classType,
   characterAttributes,
 }: ClassesProps) {
+  const clickRef = useRef(null);
   const classEnabled = Object.entries(classAttributes).every(
     ([attribute, value]) => characterAttributes[attribute] >= value
   );
+
+  const [showRequirements, setShowRequirements] = useState(false);
+  const handleShowRequirements = (event) => {
+    if (clickRef.current && clickRef.current.contains(event.target)) {
+      return;
+    }
+    setShowRequirements(true);
+  }
   return (
     <div
       className={classNames("class", {
         "class--enabled": classEnabled,
       })}
+      onClick={handleShowRequirements}
     >
       <h3>{classType}</h3>
+
+      {showRequirements && (
+        <div className="minRequirements" ref={clickRef}>
+          <h4>Minimum Requirements</h4>
+          <ul>
+            {Object.entries(classAttributes).map(([attribute, value]) => (
+              <li key={attribute}>
+                {attribute}:{" "}
+                <span
+                  className={classNames("attribute__value", {
+                    "attribute__value--complies":
+                      characterAttributes[attribute] >= value,
+                  })}
+                >
+                  {value}
+                </span>
+              </li>
+            ))}
+          </ul>
+          <button
+            className="close"
+            onClick={() => {
+              console.log("clicked", showRequirements);
+              setShowRequirements(false);
+            }}
+          >
+            Close
+          </button>
+        </div>
+      )}
     </div>
   );
 }
